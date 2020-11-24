e2d9da7122802ffe7820adfaec4fbdb022f25409
Michelle McConville <mchelmconvl@gmail.com>
2020-11-24 15:17:01 -0600

    added router.get request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,3 +3,9 @@ const fs = require("fs");
 const router = require("express").Router();
 const noteData = require("../db/db.json");
 const { v4: uuidv4 } = require("uuid");     // UUID to create a unique ID
+
+// ROUTING (CRUD operations)
+
+// API GET Request
+router.get("/api/notes", (req, res) => res.json(noteData));
+
