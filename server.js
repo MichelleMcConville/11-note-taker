a9d7e31712aa1d256853c5952b1578285e9625e9
Michelle McConville <mchelmconvl@gmail.com>
2020-11-24 14:57:13 -0600

    made public folder accessible to client side

--- server.js
+++ server.js
@@ -15,3 +15,12 @@ const app = express();
 
 // Sets an initial port used later in the listener
 const PORT = process.env.PORT || 8080;
+
+// ==============================================================================
+// Middleware - f(x) executed during the lifecycle of a request to the Express 
+// server. Each middleware has access to the HTTP req & res for each route/path 
+// it's attached to. (Express itself is compromised wholly of middleware f(x))
+// ==============================================================================
+
+// Making the public folder accessible to the client side
+app.use(express.static("public"));
