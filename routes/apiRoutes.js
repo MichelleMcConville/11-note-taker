d61b4c4d380b0f15a28b8086939a0daa6f6e825d
Michelle McConville <mchelmconvl@gmail.com>
2020-11-24 15:14:30 -0600

    added load data

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,5 @@
-x
+// LOAD DATA: linking route(s) to a series of "data" sources.
+const fs = require("fs");
+const router = require("express").Router();
+const noteData = require("../db/db.json");
+const { v4: uuidv4 } = require("uuid");     // UUID to create a unique ID
