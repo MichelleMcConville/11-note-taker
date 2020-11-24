69e6c1ceabb33490e51396c7cab1619b8abdf526
Michelle McConville <mchelmconvl@gmail.com>
2020-11-24 16:35:27 -0600

    removed extra line spacing

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -22,5 +22,4 @@ router.get("*", (req, res) => res.sendFile(path.join(__dirname, "../public/index
 //   res.sendFile(path.join(__dirname, "../public/index.html"));
 // });
 
-
 module.exports = router;
